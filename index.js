f13cd828953a2b6d36ec8670a3f299b63f664d6f
Arlen Nighshade <alkdfalkdjflka@gmail.com>
2024-09-19 14:58:22 -0600

    5

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
-let resultBox = document.getElementById("result");
-
-function showMsg() {
-  if (isPalindrome(textInput.value)) {
-    resultBox.innerText = `${textInput.value} is a palindrome.`;
-  } else {
-    resultBox.innerText = `${textInput.value} is not a palindrome.`;
-  }
-}
-
-function purifyString(string) {
-  const pattern = /[$\s\p{P}]/gu;
-  return string.replace(pattern, "").toLowerCase();
-}
-function reverseString(string) {
-  let result = string.split("").reverse().join("").toLocaleLowerCase();
-  console.log(result);
-  return result;
-}
-
-const isPalindrome = (string) => {
-  return purifyString(string) === reverseString(purifyString(string))
-};
 
 checkBtn.addEventListener("click", () => {
-  isPalindrome(textInput.value);
-  if (purifyString(textInput.value) === "") {
+  const textInput = document.getElementById("text-input");
+  let resultBox = document.getElementById("result");
+  if (textInput.value === "") {
     alert("Please Input a value");
     return;
+  }
+  const isPalindrome = (string) => {
+    const pattern = /[$\s\p{P}]/gu;
+    const purifiedString = string.trim().replace(pattern, "").toLowerCase();
+    const reversedString = purifiedString
+      .split("")
+      .reverse()
+      .join("")
+      .toLowerCase();
+    return purifiedString === reversedString;
+  };
+  function showMsg() {
+    if (isPalindrome(textInput.value)) {
+      resultBox.innerText = `${textInput.value} is a palindrome.`;
+    } else {
+      resultBox.innerText = `${textInput.value} is not a palindrome.`;
+    }
   }
   showMsg();
   textInput.value = "";
